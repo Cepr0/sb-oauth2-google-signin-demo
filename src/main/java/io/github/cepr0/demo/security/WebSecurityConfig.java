1929064647326b4c8f7e18f931aad7869fc63742
Sergei Poznanski <cepr0@ukr.net>
2020-01-01 04:03:08 +0200

    Implement user signing up on frontend

--- src/main/java/io/github/cepr0/demo/security/WebSecurityConfig.java
+++ src/main/java/io/github/cepr0/demo/security/WebSecurityConfig.java
@@ -4,6 +4,7 @@ package io.github.cepr0.demo.security;
 import io.github.cepr0.demo.user.UserRepo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -47,5 +48,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
 	}
 
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/**").permitAll();
+	}
 }
 
