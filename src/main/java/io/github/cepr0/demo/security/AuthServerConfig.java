d880b651c5b07be137cdb84368fb04883292204f
Sergei Poznanski <cepr0@ukr.net>
2020-01-04 20:19:36 +0200

    Add unified claims mapper

--- src/main/java/io/github/cepr0/demo/security/AuthServerConfig.java
+++ src/main/java/io/github/cepr0/demo/security/AuthServerConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
@@ -19,8 +20,6 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 import java.util.List;
 import java.util.Map;
 
-import static java.util.Optional.ofNullable;
-
 @Configuration
 @EnableConfigurationProperties(AuthProps.class)
 public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
@@ -30,10 +29,12 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
 
 	private final AuthenticationManager authenticationManager;
 	private final AuthProps props;
+	private final ClaimsMapper claimsMapper;
 
 	public AuthServerConfig(AuthenticationManager authenticationManager, AuthProps props) {
 		this.authenticationManager = authenticationManager;
 		this.props = props;
+		claimsMapper = new ClaimsMapper();
 	}
 
 	@Override
@@ -50,24 +51,7 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
 		TokenEnhancerChain chain = new TokenEnhancerChain();
-		chain.setTokenEnhancers(List.of(
-				(accessToken, authentication) -> {
-					if (authentication != null && authentication.getPrincipal() instanceof CustomUserDetails) {
-						CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
-						AuthUser authUser = customUserDetails.getAuthUser();
-						Map<String, Object> additionalInfo = Map.of(
-								"user_id", authUser.getId(),
-								"user_name", authUser.getName(),
-								"user_email", authUser.getEmail(),
-								"user_avatar", ofNullable(authUser.getAvatarUrl()).orElse("")
-						);
-						((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(additionalInfo);
-					}
-					return accessToken;
-				},
-				tokenConverter()
-		));
-
+		chain.setTokenEnhancers(List.of(this::enhanceToken, tokenConverter()));
 		endpoints.authenticationManager(authenticationManager)
 				.tokenStore(tokenStore())
 				.reuseRefreshTokens(false)
@@ -78,22 +62,14 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
 	public JwtAccessTokenConverter tokenConverter() {
 		JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
 		converter.setSigningKey(props.getSigningKey());
-
 		converter.setAccessTokenConverter(new DefaultAccessTokenConverter() {
 			@Override
 			public OAuth2Authentication extractAuthentication(Map<String, ?> claims) {
 				OAuth2Authentication authentication = super.extractAuthentication(claims);
-				AuthUser authUser = new AuthUser(
-						(Integer) claims.get("user_id"),
-						(String) claims.get("user_name"),
-						(String) claims.get("user_email"),
-						(String) claims.get("user_avatar")
-				);
-				authentication.setDetails(authUser);
+				authentication.setDetails(claimsMapper.toAuthUser(claims));
 				return authentication;
 			}
 		});
-
 		return converter;
 	}
 
@@ -102,6 +78,15 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
 		return new JwtTokenStore(tokenConverter());
 	}
 
+	private OAuth2AccessToken enhanceToken(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
+		if (authentication != null && authentication.getPrincipal() instanceof CustomUserDetails) {
+			CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
+			if (accessToken instanceof DefaultOAuth2AccessToken) {
+				((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(claimsMapper.toClaims(customUserDetails));
+			}
+		}
+		return accessToken;
+	}
 }
 
 
